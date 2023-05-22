c51ffab5b74cc0ea8140fee87c5671ccd7949a08
schneider-carlo <carlo.schneider.mail@gmail.com>
2023-05-22 15:31:52 +0200

    added nuxt og image

--- app.vue
+++ app.vue
@@ -3,3 +3,10 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script lang="ts" setup>
+defineOgImageStatic({
+  title: "custom title",
+  description: "custom description",
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ["nuxt-og-image"],
+  ogImage: {
+    siteUrl: process.env.BASE_URL || "http://localhost:3000",
+  },
+});
